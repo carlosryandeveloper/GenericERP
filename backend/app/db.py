e0bd13b6a0f8179411754c9505edf9294761e5ac
Carlos Ryan <99292914+carlosryandeveloper@users.noreply.github.com>
2025-12-22 07:48:16 -0300

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,15 @@
-import os
 from sqlmodel import create_engine, Session
+from typing import Generator
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
-engine = create_engine(DATABASE_URL, echo=False)
+DATABASE_URL = "sqlite:///./genericerp.db"
 
-def get_session():
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    connect_args={"check_same_thread": False},
+)
+
+
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
